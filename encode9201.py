d32742fe743e7117ee7a579575f32f2667d83600
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-02-20 16:57:08 +0100

    Use python json.loads instead of regexes
    cleaner simpler shorter code

--- encode9201.py
+++ encode9201.py
@@ -1,20 +1,9 @@
 #!/bin/python3
 
-import re
 import sys
 import binascii
 import struct
-
-"""
-regexes annoted with a (1) are not super robust and could fail parsing
-very unusual but valid data
-Most likely this won't actually be an isue, but who knows
-"""
-number_regex = r'\s*([0-9]*\.[0-9]*)\s*'
-string_regex = r'\s*"([^"]*)"\s*' #(1)
-entire_regex = r'\{(.*)\}'        #(1)
-entry_regex  = r'([^},]*),?'      #(1)
-ending_regex = r'^\s*}'
+import json
 
 """
 takes a string without qutations marks
@@ -33,44 +22,27 @@ takes a float
 def	encode_num(data):
 	return "00" + "000000" + binascii.hexlify(struct.pack('d', float(data))).decode('utf-8').upper()
 
-"""
-takes an entry in the following format
-"field name" : value
-"""
-def	encode_entry(data):
-	key_match = re.search(r'%s:' % string_regex, data)
-	assert key_match, f"Cannot find key in {data}"
-	key = encode_str(data[key_match.start(1):key_match.end(1)])
-	assert key_match.end() + 1 < len (data), f"No value in {data}"
-	data = data[key_match.end() + 1:]
-	value_str = re.search(r'%s,?' % string_regex, data)
-	value_num = re.search(r'%s,?' % number_regex, data)
-	assert value_str or value_num, f"cannot decode {data}"
-	if (value_str):
-		value = encode_str(data[value_str.start(1):value_str.end(1)])
-	else:
-		value = encode_num(data[value_num.start(1):value_num.end(1)])
-	return key + value
+def	encode_data(data):
+	if isinstance(data, str):
+		return encode_str(data)
+	if isinstance(data, float):
+		return encode_num(data)
+	assert False, f"Cannot encode {type(data)} : {data}"
+
+def	encode_entry(key, value):
+	return encode_data(key) + encode_data(value)
 
 def	output_all(entries):
 	length = f"{len(entries):06x}".upper()
 	assert len(length) == 6, f"Error while encoding {data}, length too big"
 	print(f"9201{length}000000{''.join(entries)}")
 
-
 def	encode_all(data):
-	entries = []
-	entire_match = re.search(entire_regex, data, flags=re.DOTALL);
-	assert entire_match, "Doesnt look like good data"
-	to_encode = data[entire_match.start(1):entire_match.end(1) + 1]
-	while(1):
-		next_entry = re.search(entry_regex, to_encode, flags=re.DOTALL)
-		match_end  = re.search(ending_regex, to_encode)
-		if (match_end):
-			return output_all(entries)
-		assert next_entry, f"Malformed data at {to_encode}"
-		entries.append(encode_entry(to_encode[next_entry.start(1):next_entry.end(1)]))
-		to_encode = to_encode[next_entry.end():]
+	json_dict = json.loads(data)
+	encoded_dict = []
+	for key in json_dict:
+		encoded_dict.append(encode_entry(key, json_dict[key]))
+	output_all(encoded_dict)
 
 if __name__ == "__main__":
 	to_encode = sys.stdin.read()
