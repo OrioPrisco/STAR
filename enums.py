4ad22c96b139b89dd417ac1ace52600c48228f3a
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-10-25 23:25:21 +0200

    transform.py : add --kind option to decode with enums
    2E01 can now be dewcoded as lists of strings and rencoded as correct integer values

---
+++ enums.py
@@ -0,0 +1,113 @@
+#!/bin/python3
+
+from enum import Enum
+
+def getFromEnum(enum, value):
+	try:
+		if isinstance(value, int):
+			return enum(value)
+		if isinstance(value, str):
+			return enum[value]
+	except KeyError:
+		return None
+
+class Upgrade(Enum):
+	salvage = 0
+	weatherproof = 1
+	focus = 2
+	scanner = 3
+	stealth = 4
+	discount = 5
+	autobomb = 6
+	plating = 7
+	artifact = 8
+	blink = 9
+	extra_pow = 11
+	fortune = 12
+	reserves = 13
+	quickening = 15
+	scrap_runner = 16
+	capacity = 17
+
+class Keyword (Enum):
+	piercing = 0
+	ricochet = 1
+	split = 2
+	homing = 3
+	triple = 4
+	knockback = 5
+	giant = 6
+	reflective = 7
+	charges = 8
+	wider = 9
+	chain = 10
+	shotgun = 11
+	finale = 12
+	crossbeams = 13
+	twinshooter = 14
+	pulsewaves = 15
+	overheat = 16
+	shrapnel = 17
+	phasing = 18
+	surge = 19
+	bursts = 20
+	quick = 21
+	gatling = 22
+	backblast = 37
+	high = 43
+	backshot = 45
+
+"""
+missing :
+freeze
+akashic
+antimagic
+consecrated
+disrupting
+infested
+align
+swarming
+instant hit
+overcharge
+drag
+continuous
+bloodlust
+heavy
+lunge
+chariot
+instant recall
+lance
+shieldfield
+ringshor
+auto hammer
+
+all unique keywords and curses
+"""
+
+class Floor(Enum):
+	#(excavation) = 0 ??
+	#(excavation) = 1 ??
+	tutorial = 2
+	hub = 3
+	excavation = 4
+	archives = 5
+	maintenance_system = 6
+	bellows = 7
+	sanctum = 8
+	#??? = 9
+	the_conduit = 10
+	temple = 11
+	nowhere = 12
+	d13_battle = 13
+
+class Weapon(Enum):
+	olddefault = 0
+	vulcan = 1
+	fireball = 2
+	laser = 3
+	sword = 4
+	charge = 5
+	revolver = 6
+	razor = 7
+	skully = 100
+	#missing a few
--- transform.py
+++ transform.py
@@ -5,7 +5,13 @@ import decode as dc
 import argparse
 import json
 import sys
+import enums
 
+enum_types = {
+	"weapon" : enums.Weapon,
+	"keyword" : enums.Keyword,
+	"upgrade" : enums.Upgrade,
+}
 valid_headers = {"9201", "2E01"}
 
 if __name__ == "__main__":
@@ -14,10 +20,19 @@ if __name__ == "__main__":
 	parser.add_argument("-d", "--decode", required=False, action='store_true', default=False)
 	parser.add_argument("-v", "--verbose", required=False, action='store_true', default=False)
 	parser.add_argument("-t", "--type", required=False, action='store', choices=valid_headers)
+	parser.add_argument("-k", "--kind", required=False, action='store', choices=enum_types)
 	args = parser.parse_args()
+	if args.kind:
+		args.kind = enum_types[args.kind]
+	if args.kind and args.type == "9201":
+		parser.print_usage();
+		print(f"{__file__}: error: -k/--kind can only be used with 2E01")
+		exit(1)
+	if args.kind and not args.type and args.verbose:
+		print(f"{__file__}: Warning: -k/--kind can only be used with 2E01", file=sys.stderr)
 	if args.decode:
 		for line in args.input.readlines():
-			output = dc.decode_header(line, args.type, args.verbose)
+			output = dc.decode_header(line, args.type, args.verbose, args.kind)
 			print(json.dumps(output, indent=4))
 	else:
 		decoder = json.JSONDecoder();
@@ -26,7 +41,7 @@ if __name__ == "__main__":
 			json_obj, offset = decoder.raw_decode(inputs)
 			if args.verbose:
 				print(f"{offset} characters to decode", file=sys.stderr)
-			print(en.encode_header(json_obj, args.type))
+			print(en.encode_header(json_obj, args.type, args.verbose, args.kind))
 			inputs = inputs[offset:].strip()
 			if args.verbose:
 				print(f"now decoding `{inputs}`", file=sys.stderr)
