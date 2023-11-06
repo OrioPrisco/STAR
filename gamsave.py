80bfe48facdd7d24a4b5af9b3f8abeb05d62e501
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-11-06 20:20:10 +0100

    gamsave.py : load schema even when invoked from another directory
    much nicer to use this way

--- gamsave.py
+++ gamsave.py
@@ -5,6 +5,7 @@ import decode as dc
 import argparse
 import json
 import sys
+import os
 import enums
 import base64
 
@@ -37,8 +38,8 @@ line_decoders = {
 }
 
 if __name__ == "__main__":
-	schema = json.load(open("gamesave-schema.json", "r"))
 	lines = base64.b64decode("".join(open("gamesave.d13", "r").readlines()))
+	schema = json.load(open(os.path.join(script_dir, "gamesave-schema.json"), "r"))
 	lines = lines.decode("utf8").splitlines()
 	output = {}
 	for key in schema:
