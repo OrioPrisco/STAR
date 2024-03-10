aeaad0489365336ce3ec5e163c06f33f3974211c
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2024-03-10 17:54:16 +0100

    Decoding on web interface working
    need to make it more user friendly but the concept is there

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from pyweb import pydom
 import gamesave
+import json
 
 def decode_gamesave(event):
-	gamesave = pydom["#gamesave_d13"][0].value
-	print(gamesave)
-	
+	schema = json.load(open("./gamesave-schema.json", "r"))
+	gamesave_lines = pydom["#input"][0].value
+	print(gamesave_lines)
+	decoded = gamesave.decode_file(gamesave_lines, schema, False)
+	print(decoded)
+	pydom["#output"][0].html = json.dumps(decoded, indent=4)
+
