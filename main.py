6385004299121e11d72ffcdd400182e6193e34ce
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2024-03-11 13:31:39 +0100

    Add decode json Button

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ def decode_gamesave(event):
 	print(decoded)
 	pydom["#output"][0].html = json.dumps(decoded, indent=4)
 
+def encode_json(event):
+	pydom["#output"][0].html = ""
+	schema = json.load(open("./gamesave-schema.json", "r"))
+	json_lines = pydom["#input"][0].value
+	print(json_lines)
+	encoded = gamesave.encode_file(json_lines, schema, False)
+	print(encoded)
+	pydom["#output"][0].html = encoded
