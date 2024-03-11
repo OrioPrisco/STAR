1ec2f154ec52ba813984cc56420b6e74145f39d2
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2024-03-11 18:10:35 +0100

    Use element.value instead of element.html
    seems to fix a bug on firefox on github.io
    not too sure why or how

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 from pyscript import window, document
 
 def clean_output_and_errors():
-	pydom["#output"][0].html = ""
+	pydom["#output"][0].value = ""
 	nodes = document.querySelectorAll(".py-error")
 	if nodes:
 		for node in nodes:
@@ -15,11 +15,11 @@ def decode_gamesave(event):
 	schema = json.load(open("./gamesave-schema.json", "r"))
 	gamesave_lines = pydom["#input"][0].value
 	decoded = gamesave.decode_file(gamesave_lines, schema, False)
-	pydom["#output"][0].html = json.dumps(decoded, indent=4)
+	pydom["#output"][0].value = json.dumps(decoded, indent=4)
 
 def encode_json(event):
 	clean_output_and_errors()
 	schema = json.load(open("./gamesave-schema.json", "r"))
 	json_lines = pydom["#input"][0].value
 	encoded = gamesave.encode_file(json_lines, schema, False)
-	pydom["#output"][0].html = encoded
+	pydom["#output"][0].value = encoded
