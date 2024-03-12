5669c6fc00d9325ed4906a624add31a1d8d9c83c
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2024-03-12 17:29:42 +0100

    Highlight textarea when changing it's text
    Also reset the scroll to the top
    Gives the user feedback that something happened

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pyweb import pydom
 import gamesave
 import json
 from pyscript import window, document
+from js import highlight_elem
 
 def clean_output_and_errors():
 	pydom["#output"][0].value = ""
@@ -15,11 +16,17 @@ def decode_gamesave(event):
 	schema = json.load(open("./gamesave-schema.json", "r"))
 	gamesave_lines = pydom["#input"][0].value
 	decoded = gamesave.decode_file(gamesave_lines, schema, False)
-	pydom["#output"][0].value = json.dumps(decoded, indent=4)
+	textarea = document.getElementById("output")
+	textarea.value = json.dumps(decoded, indent=4)
+	textarea.scrollTop = 0
+	highlight_elem(textarea)
 
 def encode_json(event):
 	clean_output_and_errors()
 	schema = json.load(open("./gamesave-schema.json", "r"))
 	json_lines = pydom["#input"][0].value
 	encoded = gamesave.encode_file(json_lines, schema, False)
-	pydom["#output"][0].value = encoded
+	textarea = document.getElementById("output")
+	textarea.value = encoded
+	textarea.scrollTop = 0
+	highlight_elem(textarea)
