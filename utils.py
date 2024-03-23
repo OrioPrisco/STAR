93c826b921cb8395ece39175383356214ca36be4
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2024-03-23 20:06:26 +0100

    Minify_arrays: Minify certain keys only
    Only minifies arrays that have uncertain values for now, but leaves the other arrays alone

--- utils.py
+++ utils.py
@@ -23,7 +23,15 @@ class Logger:
 
 import json
 
-def minify_arrays(json_string):
+default_to_minify = [
+	"likely unlocks line 4",
+	"likely unlocks line 5",
+	"likely bestiary data line 6",
+	"likely bestiary data line 7",
+	"likely bestiary data line 8",
+]
+
+def minify_arrays(json_string, to_minify = default_to_minify):
 	start = 0
 	result = ""
 	decoder = json.JSONDecoder()
@@ -32,7 +40,15 @@ def minify_arrays(json_string):
 		if brakcet_index == -1:
 			result += json_string[start:]
 			return result
-		result += json_string[start:brakcet_index]
-		array, end_array = decoder.raw_decode(json_string, brakcet_index)
-		result += json.dumps(array)
-		start = end_array
+		name_end_index = json_string.rfind("\"", start, brakcet_index)
+		if name_end_index != -1:
+			name_begin_index = json_string.rfind("\"", start, name_end_index) + 1
+		if name_end_index == -1 or name_begin_index == -1 or json_string[name_begin_index:name_end_index] in to_minify:
+			result += json_string[start:brakcet_index]
+			array, end_array = decoder.raw_decode(json_string, brakcet_index)
+			result += json.dumps(array)
+			start = end_array
+		else:
+			array, end_array = decoder.raw_decode(json_string, brakcet_index)
+			result += json_string[start:end_array]
+			start = end_array
