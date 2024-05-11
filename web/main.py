adacf4518dab39373603046dd0c1216b89ded8a7
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2024-05-11 13:21:58 +0200

    Remove empty exception handlers
    cleaning remnants of old code

--- web/main.py
+++ web/main.py
@@ -6,26 +6,20 @@ from utils import minify_arrays
 def decode_gamesave(string, logger, test_run = False):
 	with open("./gamesave-schema.json", "rt") as f:
 		schema = json.load(f)
-	try:
-		decoded = gamesave.decode_file(string, schema, logger, test_run)
-		if test_run:
-			return
-		pretty = minify_arrays(json.dumps(decoded, indent = 4))
-		return pretty
-	except Exception as e:
-		raise e
+	decoded = gamesave.decode_file(string, schema, logger, test_run)
+	if test_run:
+		return
+	pretty = minify_arrays(json.dumps(decoded, indent = 4))
+	return pretty
 
 
 def encode_json(string, logger, test_run = False):
 	with open("./gamesave-schema.json", "rt") as f:
 		schema = json.load(f)
-	try:
-		encoded = gamesave.encode_file(string, schema, logger, test_run)
-		if test_run:
-			return
-		return encoded
-	except Exception as e:
-		raise e
+	encoded = gamesave.encode_file(string, schema, logger, test_run)
+	if test_run:
+		return
+	return encoded
 
 
 # export functions to js
